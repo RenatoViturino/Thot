de57388fccb340b63911d917cbe9abca2da990dc
Renato <reviturino@gmail.com>
2023-01-05 14:44:09 -0300

    Adicionar arquivos de projeto.

---
+++ Formularios/frmPrincipal.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace Thot_v3
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPrincipal));
+            this.btnPesquisar = new System.Windows.Forms.Button();
+            this.txtPesquisar = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.prgPesquisar = new System.Windows.Forms.ProgressBar();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.lblVersao = new System.Windows.Forms.Label();
+            this.lblPorcentagem = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.salvarArquivo = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // btnPesquisar
+            // 
+            this.btnPesquisar.Location = new System.Drawing.Point(436, 39);
+            this.btnPesquisar.Name = "btnPesquisar";
+            this.btnPesquisar.Size = new System.Drawing.Size(161, 40);
+            this.btnPesquisar.TabIndex = 9;
+            this.btnPesquisar.Text = "Pesquisar";
+            this.btnPesquisar.UseVisualStyleBackColor = true;
+            this.btnPesquisar.Click += new System.EventHandler(this.btnPesquisar_Click);
+            // 
+            // txtPesquisar
+            // 
+            this.txtPesquisar.Location = new System.Drawing.Point(119, 42);
+            this.txtPesquisar.Name = "txtPesquisar";
+            this.txtPesquisar.Size = new System.Drawing.Size(311, 35);
+            this.txtPesquisar.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 29);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Pesquisar";
+            // 
+            // prgPesquisar
+            // 
+            this.prgPesquisar.Location = new System.Drawing.Point(17, 110);
+            this.prgPesquisar.Name = "prgPesquisar";
+            this.prgPesquisar.Size = new System.Drawing.Size(580, 34);
+            this.prgPesquisar.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.prgPesquisar.TabIndex = 10;
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Location = new System.Drawing.Point(12, 157);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(141, 29);
+            this.lblInfo.TabIndex = 11;
+            this.lblInfo.Text = "Aguardando....";
+            // 
+            // lblVersao
+            // 
+            this.lblVersao.AutoSize = true;
+            this.lblVersao.Location = new System.Drawing.Point(454, 157);
+            this.lblVersao.Name = "lblVersao";
+            this.lblVersao.Size = new System.Drawing.Size(76, 29);
+            this.lblVersao.TabIndex = 13;
+            this.lblVersao.Text = "Versão";
+            // 
+            // lblPorcentagem
+            // 
+            this.lblPorcentagem.AutoSize = true;
+            this.lblPorcentagem.BackColor = System.Drawing.Color.Transparent;
+            this.lblPorcentagem.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPorcentagem.Location = new System.Drawing.Point(311, 118);
+            this.lblPorcentagem.Name = "lblPorcentagem";
+            this.lblPorcentagem.Size = new System.Drawing.Size(0, 23);
+            this.lblPorcentagem.TabIndex = 14;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(336, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 22);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "%";
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(645, 288);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblPorcentagem);
+            this.Controls.Add(this.lblVersao);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.prgPesquisar);
+            this.Controls.Add(this.btnPesquisar);
+            this.Controls.Add(this.txtPesquisar);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "frmPrincipal";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = ":__Thot__:";
+            this.Load += new System.EventHandler(this.frmPrincipal_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPesquisar;
+        private System.Windows.Forms.TextBox txtPesquisar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar prgPesquisar;
+        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.Label lblVersao;
+        private System.Windows.Forms.Label lblPorcentagem;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.SaveFileDialog salvarArquivo;
+    }
+}
+
---
+++ Formularios/frmPrincipal.cs
@@ -0,0 +1,121 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Thot_v3
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPesquisar_Click(object sender, EventArgs e)
+        {
+            string conteudo = "";
+
+            try
+            {
+                corre();     
+                conteudo = pesquisarConteudo(txtPesquisar.Text);
+                criarArquivo(conteudo);
+             }
+            catch (Exception a)
+            {
+                Console.WriteLine(a.Message);
+                throw;
+            }
+        }
+
+        private void criarArquivo(String conteudo)
+        {
+            throw new NotImplementedException();
+        }
+
+        private String pesquisarConteudo(string conteudo)
+        {
+            string google = "http://www.google.com.br/";
+            IWebDriver drive = new ChromeDriver();
+            IWebElement campoPesquisa;
+            IWebElement botaoIr;
+            IWebElement clicarLink;
+            IWebElement copiarConteudo;
+
+            try
+            {
+
+                drive.Navigate().GoToUrl(google);
+                campoPesquisa = drive.FindElement(By.Id("lst-ib"));
+                campoPesquisa.SendKeys(txtPesquisar.Text);
+
+                System.Threading.Thread.Sleep(25);
+
+                botaoIr = drive.FindElement(By.Name("btnK"));
+
+                botaoIr.Click();
+
+
+                System.Threading.Thread.Sleep(25);
+                clicarLink = drive.FindElement(By.ClassName("LC20lb"));
+                clicarLink.Click();
+
+                copiarConteudo = drive.FindElement(By.TagName("body"));
+                System.Threading.Thread.Sleep(2500);
+
+                copiarConteudo.SendKeys(OpenQA.Selenium.Keys.Control + "a");
+
+
+
+
+                return copiarConteudo.ToString();
+            }
+            catch (NoSuchElementException q)
+            {
+
+                Console.WriteLine("Erro no " + q.Message);
+                throw;
+            }
+        }
+
+        private void corre()
+        {
+            try
+            {
+                lblPorcentagem.Text = "0";
+                prgPesquisar.Value = 0;
+
+                while (prgPesquisar.Value < 100)
+                {
+
+                    prgPesquisar.Increment(1);
+                    lblPorcentagem.Text = prgPesquisar.Value.ToString();
+                    lblInfo.Text = "Carregando pesquisa";
+
+                }
+
+                timer1.Stop();
+                lblInfo.Text = "Carregado";
+
+
+            }
+            catch (Exception g)
+            {
+                Console.WriteLine("Erro no " + g.Message);
+            }
+        }
+
+        private void frmPrincipal_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
